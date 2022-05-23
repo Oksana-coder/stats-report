a4b5b8eb091b9cc95ede648adfd78ec2c6bac9f8
Oksana <oksana.palamarchuk@burda-forward>
2022-05-23 15:03:18 +0200

    Added composition solution

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
 import { CsvFileReader } from "./CsvFileReader";
+import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
 
-const reader = new CsvFileReader('football.csv');
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// Create an instance of MatchReader ad pass in smth satisfying the 'DataReader' interface;
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+let manUnitedWins = 0;
+
+for(let match of matchReader.matches) {
+  if(match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if(match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+console.log(`Man United won ${manUnitedWins} games`);
+
+/* 
+// Example of the inheritance usage
+const reader = new MatchReader('football.csv');
 reader.read();
 
 const dateOfMatch = reader.data[0][0];
@@ -16,3 +37,4 @@ for(let match of reader.data) {
   }
 }
 console.log(`Man United won ${manUnitedWins} games`);
+*/
