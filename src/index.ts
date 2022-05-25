3b554d437c3b26da178d9adb6f86a78cf9e2b8db
Oksana <oksana.palamarchuk@burda-forward>
2022-05-25 17:09:37 +0200

    Updated for composition approach

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
+import { ConsoleReport } from "./reportTargets/consoleReport";
+import { HtmlReport } from "./reportTargets/HtmlReport";
+import { Summary } from "./Summary";
 
+// Example of the composition usage 1 - static methods
+const matchReader = MatchReader.fromCsv('football.csv');
+matchReader.load();
+
+const summary = Summary.winsAnalysisWithHtmlReport('Man United', 'report3.html');
+summary.buildAndPrintReport(matchReader.matches);
+
+/*
+// Example of the composition usage 2 - regular
 // Create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader('football.csv');
 
@@ -9,16 +22,12 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUnitedWins = 0;
-
-for(let match of matchReader.matches) {
-  if(match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if(match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-console.log(`Man United won ${manUnitedWins} games`);
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new HtmlReport('manUnitedWins.html')
+);
+summary.buildAndPrintReport(matchReader.matches);
+*/
 
 /* 
 // Example of the inheritance usage
