f2ac6940097c27944ed47b55c9dd07d7c3763854
Oksana <oksana.palamarchuk@burda-forward>
2022-05-25 17:08:35 +0200

    Create new class for composition approach

---
+++ src/Summary.ts
@@ -0,0 +1,27 @@
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { MatchData } from "./MatchData";
+import { HtmlReport } from "./reportTargets/HtmlReport";
+
+export interface Analyzer {
+  run(matches: MatchData[]): string;
+}
+
+export interface OutputTarget {
+  print(report: string): void;
+}
+
+export class Summary {
+  static winsAnalysisWithHtmlReport(teamName: string, reportName: string): Summary {
+    return new Summary(
+      new WinsAnalysis(teamName),
+      new HtmlReport(reportName)
+    )
+  }
+
+  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
+
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  }
+}
